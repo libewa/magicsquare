b8549e3e300fb6e058c6972707265acd08644188
libewa <67926131+libewa@users.noreply.github.com>
2023-08-17 18:03:01 +0000

    some behind the scenes code fixes

--- Sources/magicsquare/main.swift
+++ Sources/magicsquare/main.swift
@@ -4,21 +4,26 @@ var notable = false
 
 var args = CommandLine.arguments
 args.remove(at: 0)
-if args.contains("--notable") {
+
+// check for and remove notable flags
+while args.contains("--notable") {
   notable = true
   args.remove(at: args.firstIndex(of: "--notable")!)
 }
-guard args.count <= 5 else {
+
+guard args.count <= 4 else {
   print("Please provide a maximum of 4 arguments! (\(args.count): \(args) given)")
   throw MQMagicSquareError.tooManyArgs
 }
 
 var num: [Int] = []
 
+// Get all other args into an [Int] (they come as strings)
 for element in args {
   num.append(Int(element) ?? Int.random(in: 10...100))
 }
 
+// Fill the rest of the slots with random numbers
 for _ in args.count...4 {
   num.append(Int.random(in: 10...100))
 }
