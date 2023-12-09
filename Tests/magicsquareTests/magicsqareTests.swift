db2b738c40b3f9adedca278302409bef006ecf73
libewa <67926131+libewa@users.noreply.github.com>
2023-12-09 21:00:49 +0100

    unit tests

--- Tests/magicsquareTests/magicsqareTests.swift
+++ Tests/magicsquareTests/magicsqareTests.swift
@@ -6,13 +6,41 @@ func arrSum(_ arr: [Int]) -> Int {
     accumulator + element
   }
 }
-final class Swift_Package_ManagerTests: XCTestCase {
+
+func columnSums(matrix: [[Int]]) -> [Int] {
+    guard let numRows = matrix.first?.count, numRows > 0 else {
+        return []
+    }
+
+    var columnSums = Array(repeating: 0, count: numRows)
+
+    for row in matrix {
+        guard row.count == numRows else {
+            fatalError("The matrix must have consistent row lengths")
+        }
+
+        for (index, value) in row.enumerated() {
+            columnSums[index] += value
+        }
+    }
+
+    return columnSums
+}
+
+final class magicsquareTests: XCTestCase {
     func testSquare() throws {
         let magic = MQCalcMagic(Int.random(in: 10...100), Int.random(in: 10...100), Int.random(in: 10...100), Int.random(in: 10...100))
         let rowSums: [Int] = [arrSum(magic[0]), arrSum(magic[1]), arrSum(magic[2]), arrSum(magic[3])]
+        let colSums = columnSums(matrix: magic)
+        print("""
+        +++
+        \(MQRenderSquare(magic))
+        +++
+        """)
         for x in rowSums {
-          for y in rowSums {
-            XCTAssertEqual(rowSums[x], rowSums[y])
+          for y in colSums {
+            print("Comparing \(x) with \(y)")
+            XCTAssertEqual(x, y)
           }
         }
     }
